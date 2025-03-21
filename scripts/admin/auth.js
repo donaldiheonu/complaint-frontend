a0d98d8fed466b3380081d4aa884b1e3b0fb94bd
Samuel <oluwafemisam40@gmail.com>
2025-03-21 23:04:25 +0100

    Added authentication validation for admin

---
+++ scripts/admin/auth.js
@@ -0,0 +1,33 @@
+var token = getCookie("adminToken");
+function authenticate() {
+    // get token cookie and validate
+
+    if (token == "" || token == undefined || token == null) {
+        window.location.href = "../../../admin/";
+    } else {
+        verifyToken(token);
+    }
+}
+
+function getCookie(item) {
+    var name = item + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
+function logout() {
+    document.cookie = "adminToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    window.location.href = "./";
+}
+authenticate()
